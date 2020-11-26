b3d4babba01cd0da4e7328522124ba4eca30e575
Nastya Titarenko <titnastya92@gmail.com>
2020-11-26 17:44:27 +0200

    com

---
+++ src/main/java/Main.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+enum Season {
+    WINTER, SPRING, SUMMER, AUTUMN
+}
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        Season season;
+
+        switch (input) {
+            case "December":
+            case "January":
+            case "February":
+                season = Season.WINTER;
+                System.out.println(season);
+                break;
+            case "March":
+            case "April":
+            case "May":
+                season = Season.SPRING;
+                System.out.println(season);
+                break;
+            case "June":
+            case "July":
+            case "August":
+                season = Season.SUMMER;
+                System.out.println(season);
+                break;
+            case "September":
+            case "October":
+            case "November":
+                season = Season.AUTUMN;
+                System.out.println(season);
+                break;
+            default:
+                System.out.println("No this month");
+
+        }
+
+        }
+}
